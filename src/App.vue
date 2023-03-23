f1a71526a5bacf3fd32e2c308d0415cfa6013cc2
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-03-23 02:18:17 +0100

    To Do en Vue & Bootstrap #1
    Creo un To Do simple para añadir tareas y eliminarlas.

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  components: { TodoItem },
+  data() {
+    return {
+      newTodoText: '',
+      todos: [],
+      nextTodoId: 1
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container mt-5">
+    <form class="mb-3" @submit.prevent="addNewTodo">
+      <div class="form-group">
+        <div class="input-group">
+          <input v-model="newTodoText" class="form-control" placeholder="Añade To Do...">
+          <div class="input-group-append">
+            <button class="btn btn-primary">Añadir</button>
+          </div>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group">
+      <todo-item v-for="todo in todos" :key="todo.id" :title="todo.title" @remove="todos.splice(index, 1)"></todo-item>
+    </ul>
+  </div>
+</template>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: ['title'],
+    emits: ['remove']
+}
+</script>
+
+<template>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        {{ title }}
+        <button class="btn btn-danger" @click="$emit('remove')">Eliminar</button>
+    </li>
+</template>
